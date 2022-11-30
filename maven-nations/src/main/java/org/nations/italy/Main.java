3258d231e62e582e4a9c426716a34694a53e0dc4
Marco Cafasso <marcocafassog@gmail.com>
2022-12-01 00:27:07 +0100

    feat: add query to get all nations
    created the query to get the list of all countries showing id, name, region name and continent name, sorted by country name

--- maven-nations/src/main/java/org/nations/italy/Main.java
+++ maven-nations/src/main/java/org/nations/italy/Main.java
@@ -1,10 +1,57 @@
 package org.nations.italy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		String url = "jdbc:mysql://localhost:3306/nations";
+		String user = "root";
+		String password = "root";
+		 
+		Connection con = null;
+		try {
+			
+			con = DriverManager.getConnection(url, user, password);
+			
+			final String sql = "SELECT country_id, countries.name AS 'country_name', regions.name AS 'region_name', continents.name AS 'continent_name'\r\n"
+					+ "FROM nations.countries\r\n"
+					+ "JOIN regions\r\n"
+					+ "	ON countries.region_id = regions.region_id\r\n"
+					+ "JOIN continents\r\n"
+					+ "	ON regions.continent_id = continents.continent_id\r\n"
+					+ "ORDER BY countries.name;";
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				final int id = rs.getInt(1);
+				final String name = rs.getString(2);
+				final String regionName = rs.getString(3);
+				final String continentName = rs.getString(4);
+				
+				System.out.println(id + " - " + name + " - " + regionName + " - " + continentName);
+			}
+			
+			ps.close();
+			
+		} catch (SQLException ex) {
+			
+			ex.printStackTrace();
+		} finally{
+			
+			try {
+				con.close();
+			} catch (Exception e) { }
+		}
 
 	}
 
